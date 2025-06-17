2cc359761349df6e30664ebb168ea4eeb2dcaa97
younome71 <129515402+younome71@users.noreply.github.com>
2025-06-17 16:49:41 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,4 +1,6 @@
-﻿require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
